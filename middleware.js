d2ea0f5a0eb1fc3142c24067d4d35469f0ae2943
Piyush <davidpiyush225@gmail.com>
2025-01-12 16:42:31 +0530

    fixing the cores issue

--- middleware.js
+++ middleware.js
@@ -1,9 +1,31 @@
-// Apply middleware to protect the dashboard route
-
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt"; // Import getToken to check JWT
 
 export async function middleware(request) {
+  const response = NextResponse.next();
+
+  // Set CORS headers
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    "https://news-blog-web.vercel.app"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  // Handle preflight requests (OPTIONS method)
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      headers: response.headers,
+      status: 200,
+    });
+  }
+
   // Get the session token from the request
   const token = await getToken({ req: request });
 
@@ -13,7 +35,7 @@ export async function middleware(request) {
   }
 
   // If token exists, allow the request to proceed
-  return NextResponse.next();
+  return response;
 }
 
 // Apply middleware to protect the dashboard route
